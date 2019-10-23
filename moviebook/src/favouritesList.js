37d8a10a9b931c9d3faffc548529bf2acfb9082f
Zain Kazmi <zkazmi786@hotmail.ca>
2019-10-23 18:48:11 -0400

    Complete recentlyWatched and FavouriteLists
    Completed the classes for recently watched and favourited movies. Currently consist of hardcoded values, however,
    will be replaced with dynamically generated content once a database is hooked.

---
+++ moviebook/src/favouritesList.js
@@ -0,0 +1,18 @@
+ import React from 'react'                                                                                                                                                                                                                         import { Link } from 'react-router-dom'                                                                                  import TextField from 'material-ui/TextField'                                                                            import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider'                                                       import AppBar from 'material-ui/AppBar'                                                                                                                                                                                                           class favouritesList extends React.Component {
+    state = {
+	favouritesList: ["Avengers EndGame", "The Hobbit", "The Nun", "Fifty Shades of Grey"],
+     }
+     render(){
+	return (
+	<div id="favouritesList">      
+	    {this.favouritesList.list.map(movie => (<li id={movie}>{movie}</li>))}
+	</div>
+	);
+
+     }
+ 
+ 
+ 
+ }
+
+export default favouritesList;
