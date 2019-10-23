a8a060a545995dabe37af732298ea223f9123379
Zain Kazmi <zkazmi786@hotmail.ca>
2019-10-23 18:22:55 -0400

    Create blank Component for recently watched

---
+++ moviebook/src/recently_watched.js
@@ -0,0 +1,10 @@
+ import React from 'react'                                                                                                                                                                                                                         import { Link } from 'react-router-dom'                                                                                  import TextField from 'material-ui/TextField'                                                                            import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider'                                                       import AppBar from 'material-ui/AppBar'                                                                                                                                                                                                           class recentlyWatched extends React.Component {
+     
+     render(){
+
+
+     }
+ 
+ 
+ 
+ }
