c30fddac9f623d47f948da8abe61f81d395ef237
Lina Refai <linatrefai@yahoo.com>
2019-12-01 18:31:02 -0500

    added styling to login page

--- moviebook/client/src/Views/Login.js
+++ moviebook/client/src/Views/Login.js
@@ -6,12 +6,16 @@ import {
   Card,
   Grid,
   CardContent,
+  Typography
 } from '@material-ui/core'
 import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider'
 import AppBar from 'material-ui/AppBar'
 import 'bootstrap/dist/css/bootstrap.min.css'
 import { Carousel } from 'react-bootstrap'
 import '../App.css';
+import BackgroundSlideshow from 'react-background-slideshow'
+import { spacing } from '@material-ui/system'
+
 
 
 class Login extends React.Component {
@@ -53,62 +57,41 @@ class Login extends React.Component {
     return (
       <div>
         <MuiThemeProvider>
-            <AppBar title="MovieShare"/> 
-            
-            <div style = { style.carousel }>
-              <Carousel className= "carousel" interval={3000}>
-                <Carousel.Item>
-                  <div className="carouselPic">
-                  <img className="d-block h-100" src ='../../Assets/Movie-banner.jpg' alt = "First Slide"/>
-                  </div>
-                  <Carousel.Caption>
-                    <h1>Discover what your friends are watching</h1>
-                  </Carousel.Caption>
-                </Carousel.Item>
-
-                <Carousel.Item>
-                    <img className="d-block w-100" src ='../../Assets/dunkirk-banner.jpg' alt = "Second Slide"/>
-                    <Carousel.Caption>
-                      <h1>Keep track of what to watch</h1>
-                    </Carousel.Caption>
-                </Carousel.Item>
-
-                <Carousel.Item>
-                  <img className="d-block w-100" src ='../../Assets/Joker-Banner.jpg' alt = "Third Slide"/>
-                  <Carousel.Caption>
-                    <h1>Get the hottest recommendations</h1>
-                </Carousel.Caption>
-                </Carousel.Item>
-              </Carousel>
-            </div>
-
-            <Card style={{width: '30%', height: '40%', margin: 'auto'}} raised={true}> 
-            <CardContent style={{margin: "5%"}}>
-              <TextField
+          <AppBar title="MovieShare"> 
+            <div style={{alignContent: 'right', marginTop: "-15px"}}>
+            <TextField
                   id="username"
+                  margin="dense"
                   hintText="Enter your Username"
                   floatingLabelText="Username"
                   onChange = {(event, value) => this.setState({username:value})}
-              />
-              <br/>
-              <TextField
-                  id="password"
-                  type="password"
-                  hintText="Enter your Password"
-                  floatingLabelText="Password"
-                  onChange = {(event, value) => this.setState({password:value})}
-              />
-            
-          
-              <br/>
-              <p style ={ style.login }>{this.state.error}</p>
-              <Link to={this.state.next_page} >
-                <Button onClick={ this.checkCreds } style = { style.login }>
-                  sign in
-                </Button>
-              </Link>
-            </CardContent>
-            </Card>
+                  style={{marginRight: "10px"}}
+            />
+            <TextField
+                id="password"
+                variant="outlined"
+                type="password"
+                hintText="Enter your Password"
+                floatingLabelText="Password"
+                onChange = {(event, value) => this.setState({password:value})}
+                style={{marginRight: "10px"}}
+            />
+            {/* <p style ={ style.login }>{this.state.error}</p> */}
+            <Button onClick={ this.checkCreds } style = { style.login }>
+              sign in
+            </Button>
+            </div>
+          </AppBar> 
+          <div style={{display: 'flex', flexDirection: "row", alignItems: 'center', justifyContent: 'center'}}>
+            <BackgroundSlideshow 
+              images={[ 
+                '../../Assets/Joker-Banner.jpg', 
+                '../../Assets/dunkirk-banner.jpg', 
+                '../../Assets/Movie-banner.jpg' ]}
+              animationDelay={3000} 
+              style={{height: "100vh"}}
+            /> 
+          </div>
         </MuiThemeProvider>
       </div>
     );
@@ -117,13 +100,14 @@ class Login extends React.Component {
 
 const style = {
   carousel: {
-    height: "-10%",
-    width: "-4%"
+    marginTop: "10%",
+    width: "100%",
+    height: "90%"
   },
   login: {
-    marginLeft: '37%',
-    background: 'lightblue'
-  }  
+    marginRight: '-15px',
+    background: 'clear'
+  },
 };
 
 export default Login;
